4f944b5b44283463fdfe7083e276aa3cc8feb3bf
kabo9872 <kabza.makachief@gmail.com>
2025-09-18 18:05:55 +0200

    Add files via upload

---
+++ vallidation.js
@@ -0,0 +1,32 @@
+ function validate(event) {
+
+    event.preventDefault();
+
+    const email = document.getElementById('email').value;
+    const pass = document.getElementById('password').value;
+    const age = document.getElementById('age').value;
+    const msgBox = document.getElementById('message');
+
+
+    let message = '';
+
+    if (email === '') {
+        message = 'please enter an email. ';
+        msgBox.style.color = 'red';
+    } else if (pass === '') {
+        message = 'password must be atleast 8 characters. ';
+        msgBox.style.color = 'red';
+    } else if (age ==='') {
+        message = 'must be between 12 and 50. ';
+        msgBox.style.color = 'red';
+    }
+
+    else {
+        message = 'Login successful!';
+        msgBox.style.color = 'green';
+    }
+
+    msgBox.innerText = message;
+
+
+ }
