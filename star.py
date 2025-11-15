b9c9a54b092ebefbdb5b977fe728e3cfd3998cc5
kabo9872 <kabza.makachief@gmail.com>
2025-11-15 18:08:00 +0200

    Add files via upload

---
+++ star.py
@@ -0,0 +1,28 @@
+import turtle
+
+turtle.Screen().bgcolor("purple")
+pen = turtle.Turtle()
+
+pen.forward(100)
+
+pen.left(120)
+pen.forward(100)
+
+pen.left(120)
+pen.forward(100)
+
+pen.penup()
+pen.right(150)
+pen.forward(50)
+
+pen.down()
+pen.right(90)
+pen.forward(100)
+
+pen.right(120)
+pen.forward(100)
+
+pen.right(120)
+pen.forward(100)
+
+turtle.done()
