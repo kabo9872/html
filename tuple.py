f0582a1627ee516410261882492a4452a628361d
kabo9872 <kabza.makachief@gmail.com>
2025-11-22 18:07:23 +0200

    Add files via upload

---
+++ tuple.py
@@ -0,0 +1,25 @@
+my_tuple = ()
+print(my_tuple)
+print()
+
+my_tuple = ("mouse",[1,2,3],(8,4,6))
+print(my_tuple)
+print()
+
+my_tuple = ('a','p','p','l','e')
+print(my_tuple[0])
+print(my_tuple[4])
+print()
+
+
+n_tuple = ("mouse",[8,4,6],(1,2,3))
+print(n_tuple[2][2])
+print()
+
+
+
+anewtuple = ('p','e','r','m','i','t')
+print("sliced :", anewtuple[1:4])
+
+for letter in (anewtuple):
+    print("hello",letter)
