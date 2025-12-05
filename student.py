e0e6ce35feddcae80909094b040e7b160d06234e
kabo9872 <kabza.makachief@gmail.com>
2025-12-05 18:25:21 +0200

    Add files via upload

--- student.py
+++ student.py
@@ -1,5 +1,19 @@
-class student:
-   grade = 10
-   print("hello my kabo and I am in", grade)
-   
-ob = student()
+class person:
+
+    def __init__(self,fname,lname):
+        self.firstname = fname
+        self.lastname = lname
+
+    def printname(self):
+        print(self.firstname,self.lastname)
+
+
+class student(person):
+    def __init__(self,fname,lname,year):
+        super().__init__(fname,lname)
+        self.graduationyear = year
+
+    
+studentobject = student("kabo","mosweu",2022)
+studentobject.printname()
+print(studentobject.graduationyear)
