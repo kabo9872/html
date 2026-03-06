1038bb7b2212b4087ccec4983809a4a8f095a45b
kabo9872 <kabza.makachief@gmail.com>
2026-03-06 18:14:19 +0200

    Add files via upload

---
+++ remove lines.py
@@ -0,0 +1,14 @@
+file1 = open('Codingal.txt','r')
+file2 = open('codingal updated.txt','w')
+
+for line in file1.readlines() :
+
+    if not(line.startswith('Coding')):
+
+        print(line)
+
+        file2.write(line)
+
+
+file2.close()
+file1.close()
