90a2f430c3c2a858187f97a636f84a7565891340
kabo9872 <kabza.makachief@gmail.com>
2026-04-14 18:05:44 +0200

    Add files via upload

---
+++ addbitwise.py
@@ -0,0 +1,27 @@
+#program to show the different bitwise operators
+num1 = 10
+num2 = 4
+#using AND operator
+print("num1 & num2 :",num1 & num2)
+#using OR operator
+print("\nnum1 | num2 = ",num1 | num2)
+#using NOT operator
+print("\n~num1 = ",~num1)
+#using XOR operator
+print("\n num1 ^ num2 = ",num1 ^ num2)
+
+num1 = 10
+num2 = 4
+
+#using right shift operator on num1
+print("\n num1 >> 1 = ",num1 >> 1)
+#using right shift operator on num2
+print("\n num2 >> 1 = ",num2 >> 1)
+
+num1 = 10
+num2 = 4
+
+#using left shift operator on num1
+print("\n num1 << 1 = ",num1 << 1)
+#using left shift operator on num2
+print("\n num2 << 1 = ",num2 << 1)
