e560c560fa945836b653855ba28b4a425499cc2a
kabo9872 <kabza.makachief@gmail.com>
2026-05-01 17:49:49 +0200

    Add files via upload

---
+++ swapping 2 vriales.py
@@ -0,0 +1,22 @@
+#program to swap 2 numbers without using a third ariable
+
+def swap1(a,b):
+
+    
+    a = a ^ b
+    b = a ^ b  
+    a = a ^ b
+    
+    print ("after swapping a = ",a,"b = ",b)
+
+def swap2(a,b):
+
+
+    a = (a & b) + (a | b)
+    b = a + (~b) + 1
+    a = a + (~b) + 1
+
+    print("after swapping a = ",a,"b = ",b)
+
+swap1(1,2)
+swap2(1,2)
